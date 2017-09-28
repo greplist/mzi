00c1769d3e8479f08a7914a4e4f689a7d24e318e
Anton Grabko <anton.grabko@synesis.ru>
2017-09-28 13:57:07 +0300

    lab7: fix linter

--- lab7.py
+++ lab7.py
@@ -135,7 +135,8 @@ class ECDSA(object):
 
 
 if __name__ == '__main__':
-    elliptic_curve = EllipticCurve(-3,
+    elliptic_curve = EllipticCurve(
+        -3,
         0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b,
         2**256 - 2**224 + 2**192 + 2**96 - 1,
         0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296,
